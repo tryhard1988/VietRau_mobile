5e2b5bc2149a95d4cd904625f6ce2e46dd61ac1b
Nguyen Tan Thien <tanthienhtc@gmail.com>
2025-10-02 21:28:43 +0700

    2/10/2025 mở app lên lại vẫn còn giỏ hàng

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//App.js
 import React from "react";
 import { Provider } from "react-redux";
 import { StatusBar, View, StyleSheet, Platform } from "react-native";
@@ -6,8 +5,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./store/Store";
+import { store, persistor } from "./store/Store";
 import CartScreen from "./screens/CartScreen";
 import CheckoutScreen from "./screens/CheckoutScreen";
 import BottomTabs from "./components/BottomTabs";
@@ -29,44 +29,46 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <MyStatusBar backgroundColor="#45BA7A" barStyle="light-content" />
-        <NavigationContainer>
-          <SafeAreaView style={styles.safeArea}>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-              <Stack.Screen name="Home" component={BottomTabs} />
-              <Stack.Screen
-                name="Cart"
-                component={CartScreen}
-                options={{
-                  headerShown: true,
-                  title: 'Giỏ hàng',
-                  headerStyle: { backgroundColor: '#45BA7A' },
-                  headerTintColor: '#fff', // màu chữ và nút back
-                  headerTitleAlign: 'center',
-                }}
-              />
+      <PersistGate loading={null} persistor={persistor}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <MyStatusBar backgroundColor="#45BA7A" barStyle="light-content" />
+          <NavigationContainer>
+            <SafeAreaView style={styles.safeArea}>
+              <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={BottomTabs} />
+                <Stack.Screen
+                  name="Cart"
+                  component={CartScreen}
+                  options={{
+                    headerShown: true,
+                    title: 'Giỏ hàng',
+                    headerStyle: { backgroundColor: '#45BA7A' },
+                    headerTintColor: '#fff', // màu chữ và nút back
+                    headerTitleAlign: 'center',
+                  }}
+                />
 
-              <Stack.Screen
-                name="Checkout"
-                component={CheckoutScreen}
-                options={{
-                  headerShown: true,
-                  title: 'Thanh toán',
-                  headerStyle: { backgroundColor: '#45BA7A' },
-                  headerTintColor: '#fff',
-                  headerTitleAlign: 'center',
-                }}
-              />
+                <Stack.Screen
+                  name="Checkout"
+                  component={CheckoutScreen}
+                  options={{
+                    headerShown: true,
+                    title: 'Thanh toán',
+                    headerStyle: { backgroundColor: '#45BA7A' },
+                    headerTintColor: '#fff',
+                    headerTitleAlign: 'center',
+                  }}
+                />
 
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Register" component={RegisterScreen} />
-              <Stack.Screen name="PostDetail" component={PostDetailScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+                <Stack.Screen name="PostDetail" component={PostDetailScreen} />
 
-            </Stack.Navigator>
-          </SafeAreaView>
-        </NavigationContainer>
-      </GestureHandlerRootView>
+              </Stack.Navigator>
+            </SafeAreaView>
+          </NavigationContainer>
+        </GestureHandlerRootView>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,6 +1,8 @@
-// store/Store.js
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { createNetworkMiddleware, reducer as networkReducer } from 'react-native-offline';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import productReducer from './reducers/ProductReducer';
 import cartReducer from './reducers/CartReducer';
 import userReducer from './reducers/UserReducer';
@@ -10,15 +12,30 @@ const networkMiddleware = createNetworkMiddleware({
   regexActionType: /.*\/pending$/,
 });
 
-const store = configureStore({
-  reducer: {
-    products: productReducer,
-    cart: cartReducer,
-    network: networkReducer,
-    user: userReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    [networkMiddleware, ...getDefaultMiddleware()],
+// Root reducer tổng thể
+const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+  network: networkReducer,
+  user: userReducer,
 });
 
-export default store;
+// cấu hình persist
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['cart', 'user'], // persist cart + user để auto-login
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => [
+    networkMiddleware,
+    ...getDefaultMiddleware({ serializableCheck: false }), // cần cho redux-persist
+  ],
+});
+
+export const persistor = persistStore(store);
